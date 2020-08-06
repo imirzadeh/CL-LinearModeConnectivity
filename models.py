05c4fb296c31532d5dd022dc030353ed9a62a385
Iman Mirzadeh <iman.kinder@gmail.com>
2020-08-06 15:25:42 -0700

    Add resnet 18 model

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 import torch.nn.utils.weight_norm as weightNorm
 import torch.nn.functional as F
 import torch.nn.init as init
+from utils import load_model
 
 class MLP(nn.Module):
 	"""
@@ -31,125 +32,107 @@ class MLP(nn.Module):
 
 
 
-
-class GatedMLP(nn.Module):
-	def __init__(self, hiddens=100):
-		super(GatedMLP, self).__init__()
-		self.gates = nn.ModuleDict()
-		for i in range(5):
-			gate_skeleton = nn.Sequential(OrderedDict([
-				('layer1', weightNorm(nn.Linear(784, hiddens))),
-				('act1', nn.ReLU()),
-				('layer2', weightNorm(nn.Linear(hiddens, hiddens))),
-				('act2', nn.ReLU()),
-				('layer3', weightNorm(nn.Linear(hiddens, 10))),
-			]))
-			self.gates['gate{}'.format(i+1)] =  gate_skeleton
-		# self.gates = nn.Module(OrderedDict(gates))
-
-	def forward(self, x, task_id):
-		gate = 'gate{}'.format(task_id)
-		out = self.gates[gate](x)
-		return out
-
-
-		
 def conv3x3(in_planes, out_planes, stride=1):
-	" 3x3 convolution with padding "
-	return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+	return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+					 padding=1, bias=False)
 
 
 class BasicBlock(nn.Module):
-	expansion=1
+	expansion = 1
 
-	def __init__(self, inplanes, planes, stride=1, downsample=None):
+	def __init__(self, in_planes, planes, stride=1, config={}):
 		super(BasicBlock, self).__init__()
-		self.conv1 = weightNorm(conv3x3(inplanes, planes, stride))
-		# self.bn1 = nn.BatchNorm2d(planes)
-		self.gn1 =nn.GroupNorm(inplanes, planes)
-		self.relu = nn.ReLU(inplace=True)
-		self.conv2 = weightNorm(conv3x3(planes, planes))
-		# self.bn2 = nn.BatchNorm2d(planes)
-		self.gn2 =nn.GroupNorm(inplanes, planes)
+		self.conv1 = conv3x3(in_planes, planes, stride)
+		self.conv2 = conv3x3(planes, planes)
 
-		self.downsample = downsample
-		self.stride = stride
+		self.shortcut = nn.Sequential()
+		if stride != 1 or in_planes != self.expansion * planes:
+			self.shortcut = nn.Sequential(
+				nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1,
+						  stride=stride, bias=False),
+			)
+		self.IC1 = nn.Sequential(
+			nn.BatchNorm2d(planes),
+			nn.Dropout(p=config['dropout'])
+			)
+
+		self.IC2 = nn.Sequential(
+			nn.BatchNorm2d(planes),
+			nn.Dropout(p=config['dropout'])
+			)
 
 	def forward(self, x):
-		residual = x
-
 		out = self.conv1(x)
-		out = self.gn1(out)
-		out = self.relu(out)
+		out = relu(out)
+		out = self.IC1(out)
 
-		out = self.conv2(out)
-		out = self.gn2(out)
-
-		if self.downsample is not None:
-			residual = self.downsample(x)
-
-		out += residual
-		out = self.relu(out)
-
+		out += self.shortcut(x)
+		out = relu(out)
+		out = self.IC2(out)
 		return out
 
 
+class ResNet(nn.Module):
+	def __init__(self, block, num_blocks, num_classes, nf, config={}):
+		super(ResNet, self).__init__()
+		self.in_planes = nf
 
-class Resnet(nn.Module):
+		self.conv1 = conv3x3(3, nf * 1)
+		self.bn1 = nn.BatchNorm2d(nf * 1)
+		self.layer1 = self._make_layer(block, nf * 1, num_blocks[0], stride=1, config=config)
+		self.layer2 = self._make_layer(block, nf * 2, num_blocks[1], stride=2, config=config)
+		self.layer3 = self._make_layer(block, nf * 4, num_blocks[2], stride=2, config=config)
+		self.layer4 = self._make_layer(block, nf * 8, num_blocks[3], stride=2, config=config)
+		self.linear = nn.Linear(nf * 8 * block.expansion, num_classes)
 
-	def __init__(self, block, layers, num_classes=10):
-		super(Resnet, self).__init__()
-		self.inplanes = 16
-		self.conv1 = weightNorm(nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=2, bias=False))
-		# self.bn1 = nn.BatchNorm2d(16)
-		self.gn1 = nn.GroupNorm(1, 16)
-		self.relu = nn.ReLU(inplace=True)
-		self.layer1 = self._make_layer(block, 16, layers[0])
-		self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
-		self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
-		self.avgpool = nn.AvgPool2d(8, stride=1)
-		self.fc = weightNorm(nn.Linear(64 * block.expansion, num_classes))
-
-		for m in self.modules():
-			if isinstance(m, nn.Conv2d):
-				n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-				m.weight.data.normal_(0, math.sqrt(2. / n))
-			elif isinstance(m, nn.BatchNorm2d):
-				m.weight.data.fill_(1)
-				m.bias.data.zero_()
-
-	def _make_layer(self, block, planes, blocks, stride=1):
-		downsample = None
-		if stride != 1 or self.inplanes != planes * block.expansion:
-			downsample = nn.Sequential(
-				nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
-				nn.BatchNorm2d(planes * block.expansion)
-			)
-
+	def _make_layer(self, block, planes, num_blocks, stride, config):
+		strides = [stride] + [1] * (num_blocks - 1)
 		layers = []
-		layers.append(block(self.inplanes, planes, stride, downsample))
-		self.inplanes = planes * block.expansion
-		for _ in range(1, blocks):
-			layers.append(block(self.inplanes, planes))
-
+		for stride in strides:
+			layers.append(block(self.in_planes, planes, stride, config=config))
+			self.in_planes = planes * block.expansion
 		return nn.Sequential(*layers)
 
-	def forward(self, x, task_id=None):
-		x = self.conv1(x)
-		x = self.gn1(x)
-		x = self.relu(x)
+	def forward(self, x, task_id):
+		bsz = x.size(0)
+		out = relu(self.bn1(self.conv1(x.view(bsz, 3, 32, 32))))
+		out = self.layer1(out)
+		out = self.layer2(out)
+		out = self.layer3(out)
+		out = self.layer4(out)
+		out = avg_pool2d(out, 4)
+		out = out.view(out.size(0), -1)
+		out = self.linear(out)
+		t = task_id
+		offset1 = int((t-1) * 5)
+		offset2 = int(t * 5)
+		if offset1 > 0:
+			out[:, :offset1].data.fill_(-10e10)
+		if offset2 < 100:
+			out[:, offset2:100].data.fill_(-10e10)
+		return out
 
-		x = self.layer1(x)
-		x = self.layer2(x)
-		x = self.layer3(x)
 
-		x = self.avgpool(x)
-		x = x.view(x.size(0), -1)
-		x = self.fc(x)
-
-		return x
+def ResNet18(nclasses=100, nf=20, config={'dropout': 0.05}):
+	net = ResNet(BasicBlock, [2, 2, 2, 2], nclasses, nf, config=config)
+	return net
 
 
-def Resnet20(**kwargs):
-	model = Resnet(BasicBlock, [3, 3, 3], **kwargs)
-	return model
+# if __name__ == "__main__":
+	# model = MLP(100, 10)
+	# init = load_model('./checkpoints/YOGby/init.pth')
+	# init = ResNet18()
+	# layers = init.state_dict()
+	# count = 0
+	# for layer in layers.keys():
+	# 	print(layer, '-->', layers[layer].shape, layers[layer].view(-1).reshape(layers[layer].shape).shape)
+
+	# for param in init.parameters():
+	# 	print(param.size())
+
+	# res = ResNet18()
+	# print(res.state_dict().keys())
+	# print(model.state_dict())
+	# for p in model.parameters():
+		# print(p.shape)
+
